3f02e0be3be85fbb330f285d2bb3e6b5a71eb359
Egor Staroverov <staroverov.egor.2010@gmail.com>
2024-07-25 22:35:45 +0300

    commit of my site

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ def index():
     return render_template('index.html', **context)
 
 
+@app.route('/top1')
+def top1():
+    return render_template('top1.html')
+
+
 if __name__ == '__main__':
     app.run()

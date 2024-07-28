599bfd9273c9a3698632ac40ad779a84cec8c2e8
Egor Staroverov <staroverov.egor.2010@gmail.com>
2024-07-28 17:16:34 +0300

    actor page

--- main.py
+++ main.py
@@ -16,5 +16,50 @@ def top1():
     return render_template('top1.html')
 
 
+@app.route('/top2')
+def top2():
+    return render_template('top2.html')
+
+
+@app.route('/top3')
+def top3():
+    return render_template('top3.html')
+
+
+@app.route('/top4')
+def top4():
+    return render_template('top4.html')
+
+
+@app.route('/top5')
+def top5():
+    return render_template('top5.html')
+
+
+@app.route('/top6')
+def top6():
+    return render_template('top6.html')
+
+
+@app.route('/top7')
+def top7():
+    return render_template('top7.html')
+
+
+@app.route('/top8')
+def top8():
+    return render_template('top8.html')
+
+
+@app.route('/top9')
+def top9():
+    return render_template('top9.html')
+
+
+@app.route('/top10')
+def top10():
+    return render_template('top10.html')
+
+
 if __name__ == '__main__':
     app.run()
